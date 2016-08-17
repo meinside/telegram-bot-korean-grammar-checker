4b1385c5ebda5a03776100e2d71aafeec4d8aa24
Sungjin Han <meinside@st-company.net>
2016-08-17 15:29:14 +0900

    Initial import

---
+++ README.md
@@ -0,0 +1,57 @@
+# 한글 맞춤법 검사 Telegram Bot
+
+이 봇은 Daum Kakao Corp.의 [맞춤법 검사 API](https://developers.daum.net/services/apis/grammar-checker/v1/check.json)를 활용해
+
+입력된 문장에 대해 맞춤법 검사를 수행, 그 결과를 응답해주는
+
+Go로 개발된 Telegram Bot입니다.
+
+## 설치
+
+설치를 위해 Go가 먼저 설치/설정되어 있어야 합니다.
+
+```
+$ git clone https://github.com/meinside/telegram-bot-korean-grammar-checker.git
+$ cd telegram-bot-korean-grammar-checker
+$ go build
+```
+
+## 설정
+
+### Telegram Bot 토큰 생성
+
+[가이드](https://core.telegram.org/bots#6-botfather) 참고.
+
+### 설정 파일 생성
+
+샘플 설정 파일을 복사한 뒤,
+
+```bash
+$ cp config.json.sample config.json
+```
+
+```json
+{
+	"telegram_api_token": "0123456789:abcdefghijklmnopqrstuvwyz-x-0a1b2c3d4e",
+	"daum_api_key": "abcd1234efgh567890ijklmn",
+	"monitor_interval": 1,
+	"is_verbose": false
+}
+```
+
+*telegram_api_token*, *daum_api_key* 등의 값을 본인의 설정에 맞게 수정하십시오.
+
+*monitor_interval*은 Telegram Bot API로부터 새로 받은 메시지를 가져오는 주기(초)로, 짧을수록 빠르게 응답합니다.
+
+*is_verbose*를 true로 설정하면 장황한 디버깅용 로그 메시지를 볼 수 있습니다.
+
+## 실행
+
+```bash
+$ ./telegram-bot-korean-grammar-checker
+```
+
+## License
+
+MIT
+
---
+++ main.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	bot "github.com/meinside/telegram-bot-go"
+)
+
+const (
+	ConfigFilename                 = "config.json"
+	DaumKoreanGrammarCheckerApiUrl = "https://apis.daum.net/grammar-checker/v1/check.json"
+
+	CommandStart   = "/start"
+	WelcomeMessage = "맞춤법을 검사할 문장을 입력해 주세요."
+)
+
+type config struct {
+	TelegramApiToken              string `json:"telegram_api_token"`
+	DaumApiKey                    string `json:"daum_api_key"`
+	TelegramUpdateIntervalSeconds int    `json:"monitor_interval"`
+	IsVerbose                     bool   `json:"is_verbose"`
+}
+
+// api result
+type checkResult struct {
+	Sentences []struct {
+		Sentence string `json:"sentence"`
+		Result   []struct {
+			Input   string   `json:"input"`
+			Output  string   `json:"output"`
+			Etype   string   `json:"etype"`
+			Help    []string `json:"help,omitempty"`
+			HelpMo  []string `json:"help_mo,omitempty"`
+			Example []string `json:"example,omitempty"`
+		} `json:"result"`
+	} `json:"sentences"`
+
+	// when error
+	ErrorType string `json:"errorType,omitempty"`
+	Message   string `json:"message,omitempty"`
+}
+
+// check grammar of given text
+//
+// https://developers.daum.net/services/apis/grammar-checker/v1/check.json
+func checkGrammar(apiKey, text string) (checkResult, error) {
+	client := &http.Client{}
+
+	var err error
+	var req *http.Request
+	var res *http.Response
+	if req, err = http.NewRequest("GET", DaumKoreanGrammarCheckerApiUrl, nil); err == nil {
+		q := req.URL.Query()
+		q.Add("apikey", apiKey)
+		q.Add("query", text)
+		q.Add("help", "on")
+		req.URL.RawQuery = q.Encode()
+
+		if res, err = client.Do(req); err == nil {
+			defer res.Body.Close()
+
+			var bytes []byte
+			if bytes, err = ioutil.ReadAll(res.Body); err == nil {
+				var result checkResult
+				if err = json.Unmarshal(bytes, &result); err == nil {
+					if result.ErrorType == "" {
+						return result, nil
+					} else {
+						err = fmt.Errorf("%s - %s", result.ErrorType, result.Message)
+					}
+				}
+			}
+		}
+	}
+
+	return checkResult{}, err
+}
+
+// build up result message string
+/*
+  * example:
+
+  > 이 대화의 목적은
+
+  > 내가 뭐라꼬 그랫는지 ⇨ 내가 뭐라고 그랬는지
+  뭐라꼬 ⇐ '뭐라꼬'는 '뭐라고'의 방언입니다.
+  그랫는지 ⇐ '그랫는지'의 옳은 표기는 '그랬는지'입니다.
+
+  > 이놈의 봇시키가 알지 모를지
+  봇시키가 ⇐ 맞춤법 오류가 의심되는 구절입니다.
+
+  > 그냥 테스트해 보는 거여따. ⇨ 그냥 테스트해 보는 거였다.
+  거여따. ⇐ 올바르지 않은 어미의 사용입니다. '거였다.'로 고쳐 씁니다.
+*/
+func buildResultMessage(result checkResult) string {
+	var message string
+
+	var guide, corrected string
+
+	for _, s := range result.Sentences {
+		guide = ""
+		corrected = s.Sentence
+
+		for _, r := range s.Result {
+			if r.Etype != "no_error" {
+				guide += fmt.Sprintf("%s ⇐ %s\n", r.Input, strings.Join(r.Help, " "))
+
+				corrected = strings.Replace(corrected, r.Input, r.Output, 1)
+			}
+		}
+
+		if corrected == s.Sentence { // no correction
+			message += fmt.Sprintf("*>* _%s_\n%s\n", s.Sentence, guide)
+		} else {
+			message += fmt.Sprintf("*>* _%s_ ⇨ *%s*\n%s\n", s.Sentence, corrected, guide)
+		}
+	}
+
+	return message
+}
+
+func main() {
+	var conf config
+	if file, err := ioutil.ReadFile(ConfigFilename); err == nil {
+		if err := json.Unmarshal(file, &conf); err != nil {
+			panic(err)
+		} else {
+			// XXX - check conf values
+			if conf.TelegramUpdateIntervalSeconds <= 0 {
+				conf.TelegramUpdateIntervalSeconds = 1
+			}
+		}
+	} else {
+		panic(err)
+	}
+
+	client := bot.NewClient(conf.TelegramApiToken)
+	client.Verbose = conf.IsVerbose
+
+	// get info about this bot
+	if me := client.GetMe(); me.Ok {
+		log.Printf("Bot information: @%s (%s)\n", *me.Result.Username, *me.Result.FirstName)
+
+		// delete webhook (getting updates will not work when wehbook is set up)
+		if unhooked := client.DeleteWebhook(); unhooked.Ok {
+			// wait for new updates
+			client.StartMonitoringUpdates(0, conf.TelegramUpdateIntervalSeconds, func(b *bot.Bot, update bot.Update, err error) {
+				if err == nil {
+					if update.HasMessage() {
+						// 'is typing...'
+						b.SendChatAction(update.Message.Chat.Id, bot.ChatActionTyping)
+
+						var message string
+						if update.Message.HasText() {
+							if *update.Message.Text == CommandStart { // skip /start command
+								message = WelcomeMessage
+							} else {
+								if result, err := checkGrammar(conf.DaumApiKey, *update.Message.Text); err == nil {
+									message = buildResultMessage(result)
+								} else {
+									message = fmt.Sprintf("API 호출 오류: %s", err)
+
+									log.Printf("*** failed to call api: %s", err)
+								}
+							}
+						} else {
+							message = WelcomeMessage
+						}
+
+						// send message back (with markup support)
+						if sent := b.SendMessage(
+							update.Message.Chat.Id,
+							&message,
+							map[string]interface{}{
+								"parse_mode": bot.ParseModeMarkdown,
+							},
+						); !sent.Ok {
+							log.Printf("*** failed to send message: %s\n", *sent.Description)
+						}
+					}
+				} else {
+					log.Printf("*** error while receiving update (%s)\n", err.Error())
+				}
+			})
+		} else {
+			panic("failed to delete webhook")
+		}
+	} else {
+		panic("failed to get info of the bot")
+	}
+}
